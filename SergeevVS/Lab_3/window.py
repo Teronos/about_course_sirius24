ed0c6356db7ca2b84a6f0bb47c4e07487f791ed4
KidawR <vadimser2002@mail.ru>
2024-10-30 16:19:22 +0300

    готово

--- SergeevVS/Lab_3/window.py
+++ SergeevVS/Lab_3/window.py
@@ -1,100 +1,124 @@
 import os
 import pathlib
-import sys
-
 from PIL.ImageQt import ImageQt
 from qtpy import uic
-
 import MGK
-import numpy as np
-import cv2
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QSlider, QVBoxLayout, QWidget, QFileDialog
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QSlider, QFileDialog
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap
 
-# типы переменным и многопоточность, лэйблу size добавить коэфицент сжатия
-class ViewCompressor(QMainWindow):
+class ImageCompressor:
+    def __init__(self):
+        self.file_path = None
+        self.temp_size = None
+        self.compressed_image = None
+
+    def load_image(self, file_path):
+        self.file_path = file_path
+        self.temp_size = os.path.getsize(self.file_path)
+
+    def get_size_img(self, value):
+        from PIL import Image
+        im = Image.open(self.file_path)
+        size_img = min(im.size)
+        return int(value / 100 * size_img)
+
+    def compress_image(self, compression_factors):
+        self.compressed_image = MGK.compress_image_pca(self.file_path, compression_factors)
+        return self.compressed_image
+
+    def get_file_size_ratio(self):
+        compressed_size = os.path.getsize(os.getcwd() + "/temp" + os.path.splitext(self.file_path)[1])
+        return compressed_size / self.temp_size
+
+    def get_size_difference_percentage(self):
+        compressed_size = os.path.getsize(os.getcwd() + "/temp" + os.path.splitext(self.file_path)[1])
+        return ((self.temp_size - compressed_size) / self.temp_size) * 100
+
+
+class View(QMainWindow):
     def __init__(self):
         super().__init__()
         self.path = pathlib.Path.cwd()
         uic.loadUi(self.path / r"SergeevVS/Lab_3/veiw.ui", self)
-        self.file_Path: str = None
-        self.compressed_image = None
 
+        # Создаем экземпляр ImageCompressor
+        self.image_compressor = ImageCompressor()
         # UI elements
         self.label = self.findChild(QLabel, "labels")
         self.size = self.findChild(QLabel, "size")
+        self.proc_compress = self.findChild(QLabel, "size_2")
+        self.size_difference = self.findChild(QLabel, "size_difference")  # Новый QLabel для отображения разницы
         self.sliderR = self.findChild(QSlider, "sliderR")
+        self.sliderG = self.findChild(QSlider, "sliderG")
+        self.sliderB = self.findChild(QSlider, "sliderB")
+        self.load_button = self.findChild(QPushButton, "load_button")
+        self.save_button = self.findChild(QPushButton, "save_button")
+
+        self.setup_ui()
+
+    def setup_ui(self):
         self.sliderR.setMinimum(0)
         self.sliderR.setMaximum(100)
         self.sliderR.setValue(100)
         self.sliderR.valueChanged.connect(self.update_image)
 
-        self.sliderG = self.findChild(QSlider, "sliderG")
         self.sliderG.setMinimum(0)
         self.sliderG.setMaximum(100)
         self.sliderG.setValue(100)
         self.sliderG.valueChanged.connect(self.update_image)
 
-        self.sliderB = self.findChild(QSlider, "sliderB")
         self.sliderB.setMinimum(0)
         self.sliderB.setMaximum(100)
         self.sliderB.setValue(100)
         self.sliderB.valueChanged.connect(self.update_image)
 
-        self.load_button = self.findChild(QPushButton, "load_button")
         self.load_button.clicked.connect(self.load_image)
-
-        self.save_button = self.findChild(QPushButton, "save_button")
         self.save_button.clicked.connect(self.save_image)
         self.save_button.setEnabled(False)
 
-
-
     def load_image(self):
         options = QFileDialog.Options()
-        self.file_Path, _ = QFileDialog.getOpenFileName(self, "Load Image", "", "Image Files (*.png *.jpg *.bmp)",
-                                                        options=options)
-        self.file_extension = os.path.splitext(self.file_Path)[1]
-        self.Temp_size = os.path.getsize(self.file_Path)
-        print(self.file_Path)
-        if self.file_Path:
+        file_path, _ = QFileDialog.getOpenFileName(self, "Load Image", "", "Image Files (*.png *.jpg *.bmp)",
+                                                   options=options)
+        if file_path:
+            self.image_compressor.load_image(file_path)
             self.update_image()
 
-    def get_size_img(self, value):
-        self.file_Path
-        return int(value/100 * size_img)
-
     def update_image(self):
-        if self.file_Path is None:
-            print("Non name")
+        if self.image_compressor.file_path is None:
+            print("No image loaded")
             return
-        compression_factor = [self.sliderR.value(), self.sliderG.value(), self.sliderB.value()]
-        self.compressed_image = MGK.compress_image_pca(self.file_Path, compression_factor)
-        self.compressed_image.save(os.getcwd() +"/temp"+self.file_extension)
-        file_size = os.path.getsize(os.getcwd()+"/temp"+self.file_extension)/self.Temp_size
-        self.size.setText(str(round(file_size, 1)))
-        self.show_image(self.compressed_image)
+
+        compression_factors = [
+            self.image_compressor.get_size_img(self.sliderR.value()),
+            self.image_compressor.get_size_img(self.sliderG.value()),
+            self.image_compressor.get_size_img(self.sliderB.value())
+        ]
+
+        self.image_compressor.compress_image(compression_factors)
+        self.image_compressor.compressed_image.save(
+            os.getcwd() + "/temp" + os.path.splitext(self.image_compressor.file_path)[1])
+
+        file_size_ratio = self.image_compressor.get_file_size_ratio()
+        self.size.setText(str(round(file_size_ratio, 1)))
+
+
+        # Вычисляем и отображаем процент изменения размера
+        size_difference_percentage = self.image_compressor.get_size_difference_percentage()
+        self.proc_compress.setText(f"{round(size_difference_percentage, 2)}%")  # Отображаем разницу
+
+        self.show_image(self.image_compressor.compressed_image)
         self.save_button.setEnabled(True)
 
     def show_image(self, image):
-        # Конвертирование изображения из BGR в RGB
         pixmap = QPixmap.fromImage(ImageQt(image))
         self.label.setPixmap(pixmap.scaled(self.label.size(), Qt.KeepAspectRatio))
 
     def save_image(self):
-        if self.compressed_image is not None:
+        if self.image_compressor.compressed_image is not None:
             options = QFileDialog.Options()
-            file_name, _ = QFileDialog.getSaveFileName(self, "Save Image", "",
-                                                       options=options)
+            file_name, _ = QFileDialog.getSaveFileName(self, "Save Image", "", options=options)
             if file_name:
-                print(file_name)
-                self.compressed_image.save(file_name+self.file_extension)
-
-
-# Запуск приложения
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = ViewCompressor()
-    window.show()
-    sys.exit(app.exec_())
+                self.image_compressor.compressed_image.save(
+                    file_name + os.path.splitext(self.image_compressor.file_path)[1])
