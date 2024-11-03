8fa32bc170f1e4ed1232e82fe7aaa0aba3245c28
hizhnyack <khizhnyak.yp@gmail.com>
2024-11-03 15:49:15 +0300

    Add files via upload

--- Intro/Intro_7.py
+++ Intro/Intro_7.py
@@ -21,12 +21,12 @@ else:
     for count in range(0,n):
         list_n.append(int(random.uniform(-1,1)*1000))
 for count in range(0, n):
-    if list_n[count]%8 == 0 and (len(str(list_n[count])) == 2 or (list_n[count] < 0 and len(str(list_n[count])) == 3)):
+    if list_n[count]%8 == 0 and ((len(str(list_n[count])) == 2 and (list_n[count] > 0)) or (len(str(list_n[count])) == 3 and list_n[count] < 0)):
         summ += list_n[count]
 print(f"Сумма двузначных чисел, кратных 8 составляет: ",summ)
 if input("Вывести последовательность для проверки результата (y/n)?  ") == "y":
     print (list_n)
-if input("Вывести числа, кратные 8 (y/n)?  ") == "y":
+if input("Вывести двузначные числа, кратные 8 (y/n)?  ") == "y":
     for count in range(0, n):
-            if list_n[count]%8 == 0 and (len(str(list_n[count])) == 2 or (list_n[count] < 0 and len(str(list_n[count])) == 3)):
+            if list_n[count]%8 == 0 and (len(str(list_n[count])) == 2 and (list_n[count] > 0)) or (len(str(list_n[count])) == 3 and list_n[count] < 0):
                 print(list_n[count])
