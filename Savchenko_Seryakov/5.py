7ef71746b7bbcbc73d0f1603f420a6be1b6f4879
seryakoff <seryakoff@internet.ru>
2024-11-20 22:47:11 +0300

    Add class Intervals, and its methods parser, weight.

--- Savchenko_Seryakov/5.py
+++ Savchenko_Seryakov/5.py
@@ -1,9 +1,12 @@
 """
 
-Мини-библиотека реализующая интервалы, с функционалом их объединения,
-вычисления ширины, проверки на эквивалентность.
-В состав бибилиотеки должен также входить парсер,
-который считывает строковое представление интеравлов
+Мини-библиотека реализующая интервалы,
+с функционалом их
+объединения,
+вычисления ширины,
+проверки на эквивалентность.
+В состав библиотеки должен также входить
+парсер, который считывает строковое представление интервалов
 и по нему создает нужные объекты.
 
 Примеры работы с библиотекой:
@@ -14,19 +17,34 @@
 
 """
 
+# TODO вывод точек,
+#  констреинс,
+#  сравнение,
+#  приватный класс
+
+# TODO класс Интервалы,
+#       функции:
+#  V    парсер9строка на подстроки0
+#  V    ширина как сумма,
+#  V    add,
+#       union, -> упрощение входных интервалов -> проход по всему листу, максимальное объединение так чтобы хранилось максимально необъединяемые
+#       эквивалентность -> упрощаем по юниону -> Сравнить по длине -> поэлементное сравнение Intervals -
+#       вывод (?3), красивый пересечением интервалов*(доработка)
+
+
 from enum import Enum
 
 
 class TypeOpenBracket(Enum):
     ROUND = '('
     SQUARE = '['
-    CURLY_OPEN = '{'
+    CURLY = '{'
 
 
 class TypeClosedBracket(Enum):
     ROUND = ')'
     SQUARE = ']'
-    CURLY_OPEN = '}'
+    CURLY = '}'
 
 
 class Interval():
@@ -37,83 +55,194 @@ class Interval():
 
     # Инициализируем
     def __init__(self, left_bracket, left_border, right_border, right_bracket):
+        # def __init__(self, input_interval: str):
         # TODO: подумать над проверкой скобок подходящих в enum
         """
         if not isinstance(left_bracket, TypeOpenBracket) or not isinstance(right_bracket, TypeClosedBracket):
             raise ValueError('Левые и правые границы должны быть экземплярами Type_Bracket.')
         """
+
+        # Код от чата:
+        # TODO Проверяем, что переданы корректные скобки
+        """
+        if left_bracket not in {b.value for b in TypeOpenBracket}:
+            raise ValueError(f"Некорректная левая скобка: {left_bracket}")
+        if right_bracket not in {b.value for b in TypeClosedBracket}:
+            raise ValueError(f"Некорректная правая скобка: {right_bracket}")
+        """
+
         self.left_bracket = str(left_bracket)
         self.left_border = int(left_border)
         self.right_border = int(right_border)
         self.right_bracket = str(right_bracket)
+
         # Если левая граница больше правой
         if self.left_border > self.right_border:
             # raise ValueError('Неверно заданы границы интервалов')
             print('Неверно заданы границы интервалов')
 
+    #
+    # Декоратор для создания методов класса
+    # Используем для создания объектов (Для альтернативного способа инициализации)
+    #
+    # Парсер интервалов
+    @classmethod
+    def parser(cls, interval_str: str):
+        # Считываем интервал
+        if interval_str[0] == '[' or interval_str[0] == '(':
+            left_bracket = interval_str[0]
+            left_border = ''
+            right_border = ''
+            i = 1
+            while interval_str[i] != ',':
+                left_border += interval_str[i]
+                i += 1
+            i += 2
+            while interval_str[i] != ']' and interval_str[i] != ')':
+                right_border += interval_str[i]
+                i += 1
+            right_bracket = interval_str[i]
+
+        # Считываем точку и переводим её в интервал
+        elif interval_str[0] == '{':
+            left_bracket = '['
+            left_border = ''
+            right_border = ''
+            i = 1
+            while interval_str[i] != '}':
+                left_border += interval_str[i]
+                i += 1
+            right_border = left_border
+            right_bracket = ']'
+        return cls(left_bracket, left_border, right_border, right_bracket)
+
     # Выводим интервал
-    def __str__(self) -> None:
-        return f'{self.left_bracket}{self.left_border}, {self.right_border}{self.right_bracket}'
+    def __str__(self) -> str:
+        if self.left_border == self.right_border and self.left_bracket == '[' and self.right_bracket == ']':
+            return f'{TypeOpenBracket.CURLY.value}{self.left_border}{TypeClosedBracket.CURLY.value}'
+        else:
+            return f'{self.left_bracket}{self.left_border}, {self.right_border}{self.right_bracket}'
 
     # Преобразуем интервал в строку
-    def __repr__(self) -> None:
-        return f'{self.left_bracket}{self.left_border}, {self.right_border}{self.right_bracket}'
-
-    def __add__(self):
-        pass
+    def __repr__(self) -> str:
+        if self.left_border == self.right_border and self.left_bracket == '[' and self.right_bracket == ']':
+            return f'{TypeOpenBracket.CURLY.value}{self.left_border}{TypeClosedBracket.CURLY.value}'
+        else:
+            return f'{self.left_bracket}{self.left_border}, {self.right_border}{self.right_bracket}'
 
     # Ширина интервала
     def weight(self):
         return self.right_border - self.left_border
 
+    # Пока для []
+    def __add__(self, other):
+        # Берём тот интервал, у которого левая граница меньше
+        if self.left_border > other.left_border:
+            self, other = other, self
 
-# Считываем интервал
-def read_int(st: str):
-    if st[0] == '[' or st[0] == '(':
-        left_bracket = st[0]
-        left_border = ''
-        right_border = ''
-        i = 1
-        while st[i] != ',':
-            left_border += st[i]
-            i += 1
-        i += 2
-        while st[i] != ']' and st[i] != ')':
-            right_border += st[i]
-            i += 1
-        right_bracket = st[i]
-        interval = Interval(left_bracket, left_border, right_border, right_bracket)
-        return interval
+        # Есть пересечение второго интервала с первым
+        if self.right_border > other.left_border:
+            # Есть включение второго интервала в первый
+            if self.right_border > other.right_border:
+                return self
+            return Interval(self.left_bracket, self.left_border, other.right_border, other.right_bracket)
 
-    # Считываем точку и переводим её в интервал
-    elif st[0] == '{':
-        left_bracket = '['
-        left_border = ''
-        right_border = ''
-        i = 1
-        while st[i] != '}':
-            left_border += st[i]
-            i += 1
-        right_border = left_border
-        right_bracket = ']'
-        interval = Interval(left_bracket, left_border, right_border, right_bracket)
-        return interval
+        elif self.right_border == other.left_border:
+            # if self.right_bracket == TypeClosedBracket.SQUARE or other.left_bracket == TypeOpenBracket.SQUARE:
+            if self.right_bracket == ']' or other.left_bracket == '[':
+                return Interval(self.left_bracket, self.left_border, other.right_border, other.right_bracket)
+            else:
+                # return [self, other]
+                intervals = Intervals()
+                intervals.__add__(self)
+                intervals.__add__(other)
+                return intervals
+
+        # return [self, other]
+        intervals = Intervals()
+        intervals.__add__(self)
+        intervals.__add__(other)
+        return intervals
 
 
-# Добавление точек
-def Add(str):
-    pass
+class Intervals():
+    list_intervals: list
 
+    # list_intervals: list[Interval]
 
-# Объединение интервалов
-def Merger(str):
-    pass
+    def __init__(self):
+        self.list_intervals = []
+        # list_intervals = [self]
+        # list_intervals.append[self]
+
+    @classmethod
+    def parser(cls, intervals_srt):
+        intervals = cls()
+        new_intervals_srt = intervals_srt[1:-1]
+        # parts = new_intervals_srt.split(', ')
+        # for i in range(len(new_intervals_srt)):
+        while len(new_intervals_srt) > 1:
+            i = 0
+            while new_intervals_srt[i] != ']' and new_intervals_srt[i] != ')' and new_intervals_srt[i] != '}':
+                i += 1
+            part = new_intervals_srt[:i + 1]
+            inter = Interval.parser(part)
+            intervals.list_intervals.append(inter)
+            if len(new_intervals_srt[i + 1:]) > 3:
+                new_intervals_srt = new_intervals_srt[i + 3:]
+            else:
+                new_intervals_srt = new_intervals_srt[i + 1:]
+
+        return intervals
+
+    # TODO - подумать как оно должно работать (это довавление а не сумма)
+    def __add__(self, interval):
+        self.list_intervals.append(interval)
+
+    def __str__(self):
+        output_inter = '['
+        for inter in self.list_intervals:
+            output_inter += inter.__repr__() + ', '
+
+        output_inter = output_inter[:-2]
+        output_inter += ']'
+        return output_inter
+
+    def __repr__(self):
+        output_inter = '['
+        for inter in self.list_intervals:
+            output_inter += inter.__repr__() + ', '
+
+        output_inter = output_inter[:-2]
+        output_inter += ']'
+        return output_inter
+
+    def weight(self):
+        sum_weight = 0
+        for interv in self.list_intervals:
+            sum_weight += interv.weight()
+        return sum_weight
 
 
 if __name__ == '__main__':
-    inter = Interval('(', '2', '4', ')')
-    print(inter)
-    print(inter.weight())
-    print(read_int('[52, 34]'))
-    print(read_int('[-123, 3)'))
-    print(read_int('{-123}'))
+    # interval1 = Interval.parser('[10, 12]')
+    interval1 = Interval.parser('{10}')
+    print(interval1)  # Вывод: [7, 8]
+    print(interval1.weight())
+    interval2 = Interval.parser('(-2, 5)')
+    # interval2 = Interval.parser('{4}')
+    print(interval2)  # Вывод: [5, 13]
+    print(interval2.weight())
+
+    interval3 = interval1 + interval2
+    print(interval3)
+    print(interval3.weight())
+
+    interval4 = Intervals.parser('[(0, 1), (1, 7), (7, 10]]')
+    print(interval4)
+    print(type(interval4))
+
+    interval5 = Intervals.parser('[(0, 1), (1, 7), {10}]')
+    print(interval5)
+    print(type(interval5))
+
